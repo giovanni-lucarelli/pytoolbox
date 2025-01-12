2c71f216d4eaf96dcb478a11b75af600f5a2c7d2
Giovanni Lucarelli <gio.lucarelli99@gmail.com>
2025-01-12 13:26:30 +0100

    new version. TODO: include elena's code

---
+++ mytoolbox/interpolation.py
@@ -0,0 +1,119 @@
+import sys
+sys.path.append('../build')
+import interpolation_bindings # Importa el módulo que contiene los bindings de C++
+import numpy as np
+import time
+from scipy.interpolate import CubicSpline
+
+
+class Interpolator: 
+    def __init__(self, x_nodes, y_nodes):
+
+        self.x_nodes = x_nodes 
+        self.y_nodes = y_nodes
+        
+        # Create the instances of the interpolators (using the classes exposed by pybind11)
+        self.linear_interpolator = interpolation_bindings.LinearInterpolator(x_nodes, y_nodes)
+        self.newton_interpolator = interpolation_bindings.NewtonInterpolator(x_nodes, y_nodes)
+        self.gsl_polynomial_interpolator = interpolation_bindings.GslPolynomialInterpolator(x_nodes, y_nodes)
+        self.cubic_spline_interpolator = interpolation_bindings.CardinalCubicSpline(x_nodes, y_nodes, x_nodes[0], x_nodes[1]-x_nodes[0] )  # Por ejemplo, los parámetros pueden variar según tu implementación
+
+    def linear_interpolate(self, x):
+
+        return self.linear_interpolator(x)
+
+    def newton_interpolate(self, x):
+
+        return self.newton_interpolator(x)
+    
+    def gsl_polynomial_interpolate(self, x):
+
+        return self.gsl_polynomial_interpolator(x)
+
+    def cubic_spline_interpolate(self, x):
+        
+        return self.cubic_spline_interpolator(x)
+    
+def calculate_errors(real_values, interpolated_values):
+    absolute_errors = np.abs(real_values-interpolated_values)
+    relative_errors = np.abs(absolute_errors/real_values)
+    mae = np.mean(absolute_errors)
+    return absolute_errors, relative_errors, mae
+
+def generate_data(func, x_range, num_points, exclude_extremes = False):
+    if exclude_extremes:
+        epsilon = 1e-5
+        x = np.linspace(x_range[0] + epsilon, x_range[1] - epsilon, num_points)
+    else:
+        x = x = np.linspace(x_range[0], x_range[1], num_points)
+    y = func(x)
+    return x, y
+
+def measure_execution_time(func, *args, **kwargs):
+    """
+    Measures the execution time of the function passed as an argument.
+
+    Parameters:
+    - func: the function to be executed.
+    - *args: positional arguments that the function may require.
+    - **kwargs: keyword arguments that the function may require.
+
+    Returns:
+    - The execution time in seconds.
+    """
+    start_time = time.time() 
+    func(*args, **kwargs) 
+    end_time = time.time() 
+    
+    return end_time - start_time  
+
+# Implementing interpolations in pure Python (I think it is unnecessary)
+def linear_interpolation(x_nodes, y_nodes, x):
+    """
+    Realiza la interpolación lineal para un valor x dado, usando nodos x_nodes y valores y_nodes.
+    """
+    n = len(x_nodes)
+    for i in range(n - 1):
+        if x_nodes[i] <= x <= x_nodes[i + 1]:
+            return y_nodes[i] + (x - x_nodes[i]) * (y_nodes[i + 1] - y_nodes[i]) / (x_nodes[i + 1] - x_nodes[i])
+    return None
+def newton_interpolation(x_nodes, y_nodes, x):
+    """
+    Realiza la interpolación de Newton para un valor x dado.
+    """
+    n = len(x_nodes)
+    diff_table = np.zeros((n, n))
+    diff_table[:, 0] = y_nodes
+
+    # Construir la tabla de diferencias divididas
+    for j in range(1, n):
+        for i in range(n - j):
+            diff_table[i, j] = (diff_table[i + 1, j - 1] - diff_table[i, j - 1]) / (x_nodes[i + j] - x_nodes[i])
+
+    # Evaluar el polinomio de Newton
+    result = diff_table[0, 0]
+    for j in range(1, n):
+        term = diff_table[0, j]
+        for i in range(j):
+            term *= (x - x_nodes[i])
+        result += term
+    return result
+def lagrange_interpolation(x_nodes, y_nodes, x):
+    """
+    Realiza la interpolación de Lagrange para un valor x dado.
+    """
+    n = len(x_nodes)
+    result = 0
+    for i in range(n):
+        term = y_nodes[i]
+        for j in range(n):
+            if j != i:
+                term *= (x - x_nodes[j]) / (x_nodes[i] - x_nodes[j])
+        result += term
+    return result
+def cubic_spline_interpolation(x_nodes, y_nodes, x):
+    """
+    Realiza la interpolación de splines cúbicos para un valor x dado.
+    """
+    cs = CubicSpline(x_nodes, y_nodes)
+    return cs(x)
--- setup.py
+++ setup.py
@@ -5,7 +5,11 @@ gsl_include_dirs = ["/usr/local/include", "/usr/include"]  # Adjust paths
 
 cpp_extensions = [
     Extension(
+<<<<<<< HEAD
         "dataframe_bindings",
+=======
+        "mytoolbox.data_frame",
+>>>>>>> fef7517 (new version. TODO: include elena's code)
         sources=["bindings/DataFrameBindings.cpp", "src/DataFrame.cpp"],
         include_dirs=["include"] + gsl_include_dirs,
         library_dirs=gsl_library_dirs,
@@ -13,7 +17,11 @@ cpp_extensions = [
         language="c++",
     ),
     Extension(
+<<<<<<< HEAD
         "interpolation_bindings",
+=======
+        "mytoolbox.interpolation_bindings",
+>>>>>>> fef7517 (new version. TODO: include elena's code)
         sources=["bindings/InterpolationBindings.cpp", "src/Interpolator.cpp"],
         include_dirs=["include"] + gsl_include_dirs,
         library_dirs=gsl_library_dirs,
@@ -22,15 +30,32 @@ cpp_extensions = [
     ),
 ]
 
+<<<<<<< HEAD
 # Define the setup
 setup(
     name="pytoolbox",
+=======
+# Read long description from README.md
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+# Define the setup
+setup(
+    name="mytoolbox",
+>>>>>>> fef7517 (new version. TODO: include elena's code)
     version="0.1.0",
     author="Your Name",
     author_email="your.email@example.com",
     description="A Python package for interpolation and data manipulation using C++ libraries",
+<<<<<<< HEAD
     packages=find_packages(where="pytoolbox"),  # Look for packages in pytoolbox directory
     package_dir={"": "pytoolbox"},  # Points to the python folder as 
+=======
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/mytoolbox",  # Update with your repo
+    packages=find_packages(),
+>>>>>>> fef7517 (new version. TODO: include elena's code)
     ext_modules=cpp_extensions,
     install_requires=["numpy", "pandas", "seaborn", "matplotlib", "scipy"],
     python_requires=">=3.7",
